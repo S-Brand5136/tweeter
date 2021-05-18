4c927358d370816c16357cafcb78628c7272589a
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-18 21:57:26 +0000

    add eventListener to update output counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,11 @@
-console.log('hello world its loaded');
+$(document).ready(() => {
+  $('#tweet-text').on('input', function () {
+    const textAreaValue = $(this).val().length;
+    const output = $(this).siblings().find('output');
+    const remaingChars = 140 - textAreaValue;
+
+    remaingChars < 0 ? output.addClass('error') : output.removeClass('error');
+
+    output.val(remaingChars);
+  });
+});
