fd2a7a705618fefc3e0302ce4d2f5ef258f43b06
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-19 18:40:06 +0000

    update variable names in composer-char-counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
   $('#tweet-text').on('input', function () {
-    const textAreaValue = $(this).val().length;
-    const output = $(this).siblings().find('output');
-    const remaingChars = 140 - textAreaValue;
+    const $textAreaValue = $(this).val().length;
+    const $output = $(this).siblings().find('output');
+    const remaingChars = 140 - $textAreaValue;
 
-    remaingChars < 0 ? output.addClass('error') : output.removeClass('error');
+    remaingChars < 0 ? $output.addClass('error') : $output.removeClass('error');
 
-    output.val(remaingChars);
+    $output.val(remaingChars);
   });
 });
